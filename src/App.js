6c9ce970f2ee74fe839f19388938ce2b9854dc21
annant05 <annantguptaneema@gmail.com>
2021-05-14 18:45:47 +0000

    Learned state and basic components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// class RegularClass { }
+// class ComponentClass extends Component { }
+
+// const regularClassInstance = new RegularClass();
+// const componentClassInstance = new ComponentClass();
+
+// console.log('regularClassInstance', regularClassInstance);
+// console.log('componentClassInstance', componentClassInstance);
+
+class App extends Component {
+    state = { displayBio: false };
+    // constructor() {
+    //     super();
+    //     this.state = { displayBio: false };
+    //     console.log('Component this', this);
+
+    //     // this.readMore = () => {
+    //     //     console.log('readMore this', this);
+    //     //     this.setState({ displayBio: true });
+
+    //     // }
+    //     //  learn about bind method.
+    //     this.toggleDisplayBio = this.toggleDisplayBio.bind(this)
+    // }
+
+    toggleDisplayBio = () => {
+        this.setState({ displayBio: !this.state.displayBio });
+    }
+    render() {
+        // const bio = 
+
+        return (
+            <div>
+                <h1>Hello! </h1>
+                <p>My name is Annant, I am learning React.</p>
+                <p>I am investing my time to learn something new. Wish me luck!</p>
+                {
+                    this.state.displayBio ? (
+                        <div>
+                            <p>I live in 42, Baker street and I learn everyday.</p>
+                            <p>I use Python and JavaScript to create Apps. </p>
+                            <p>Besides coding, I also love music and science videos.</p>
+                            <button onClick={this.toggleDisplayBio}> Show Less </button>
+                        </div>
+                    ) : (
+                        <div>
+                            <button onClick={this.toggleDisplayBio}>Read more</button>
+                        </div>
+                    )
+                }
+            </div>
+        )
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// class Animal {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+//     speak() {
+//         console.log(`I am ${this.name} and I am ${this.age} years old`);
+//     }
+// }
+
+
+// const anima1 = new Animal('Simba', 3);
+// anima1.speak();
+// console.log(anima1);
+
+// class Lion extends Animal {
+//     constructor(name, age, furColor, speed) {
+//         super(name, age);
+//         this.furColor = furColor;
+//         this.speed = speed;
+//     }
+//     roar() {
+//         console.log(`ROOOAR! I have ${this.furColor} fur, and I can run ${this.speed} miles an hour!`);
+//     }
+// }
+
+// const lion1 = new Lion('Mufasa', 20, 'golden', 25);
+// lion1.speak();
+// lion1.roar();
