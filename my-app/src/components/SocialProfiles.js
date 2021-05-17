0469c7ace3689024cde67667cc6595c128bb1c67
annant05 <annantguptaneema@gmail.com>
2021-05-17 18:30:28 +0000

    learned about stateless components

--- my-app/src/components/SocialProfiles.js
+++ my-app/src/components/SocialProfiles.js
@@ -1,38 +1,31 @@
 import React, { Component } from 'react';
 import SOCIAL_PROFILES from '../data/socialProfiles';
 
-class SocialProfile extends Component {
-    render() {
+const SocialProfile = props => {
+    const { link, image } = props.socialprofile;
 
-        const { link, image } = this.props.socialprofile;
-
-        return (
-            <span >
-                <a href={link}>
-                    <img src={image} alt='social-profile' style={{ width: 35, height: 35, margin: 10 }} />
-                </a>
-            </span>
-        )
-    }
+    return (
+        <span >
+            <a href={link} target="_blank">
+                <img src={image} alt='social-profile' style={{ width: 35, height: 35, margin: 10 }} />
+            </a>
+        </span>
+    )
 }
 
-class SocialProfiles extends Component {
-    render() {
-        return (
-            <div>
-                <h2>Connect with me!</h2>
-                <div>
-                    {
-                        SOCIAL_PROFILES.map(SOCIAL_PROFILE => {
-                            return (
-                                <SocialProfile key={SOCIAL_PROFILE.id} socialprofile={SOCIAL_PROFILE} />
-                            );
-                        })
-                    }
-                </div>
-            </div>
-        )
-    }
-}
+const SocialProfiles = () => (
+
+    <div>
+        <h2>Connect with me!</h2>
+        <div>
+            {
+                SOCIAL_PROFILES.map(SOCIAL_PROFILE =>
+                (
+                    <SocialProfile key={SOCIAL_PROFILE.id} socialprofile={SOCIAL_PROFILE} />
+                ))
+            }
+        </div>
+    </div>
+)
 
 export default SocialProfiles;
