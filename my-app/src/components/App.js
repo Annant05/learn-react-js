1dad32b86b43af33449dde28604134fd203de57d
annant05 <annantguptaneema@gmail.com>
2021-05-18 20:40:45 +0000

    Added input box and keypress handler

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+    state = { artistQuery: '' };
+
+    updateArtistQuery = event => {
+        console.log('event.target.value', event.target.value);
+        this.setState({ artistQuery: event.target.value });
+    }
+
+    searchArtist = () => {
+        console.log(this.state, this.state);
+    }
+
+    handleKeyPress = event => {
+        if (event.key === 'Enter') {
+            this.searchArtist();
+        }
+    }
     render() {
 
         return (
-            <div>React App</div>
+            <div>
+                <h2>React App</h2>
+                <input
+                    onChange={this.updateArtistQuery}
+                    onKeyPress={this.handleKeyPress}
+                    placeholder="Search for the an Artist"
+                />
+                <button onClick={this.searchArtist} >Search</button>
+            </div>
         );
     }
 }
