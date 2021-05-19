84af8598ba88b054096eb7cc6b06979318bdbde2
annant05 <annantguptaneema@gmail.com>
2021-05-19 07:53:35 +0000

    Added MusicMaster as a component in portfolio app

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Router, Switch, Route } from 'react-router-dom';
 import App from './components/App';
 import Jokes from './components/Jokes';
+import MusicMaster from './projects/music-master'
 import createBrowserHistory from 'history/createBrowserHistory';
 import Header from './components/Header';
 import './index.css';
@@ -13,6 +14,7 @@ ReactDOM.render(
         <Switch>
             <Route exact path='/' render={() => <Header><App /></Header>} />
             <Route path='/jokes' render={() => <Header><Jokes /></Header>} />
+            <Route path='/music-master' render={() => <Header><MusicMaster /></Header>} />
         </Switch>
     </Router>,
     document.getElementById('root')
